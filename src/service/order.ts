749ad68e05d643c5338a2fa35b49e1a8bd83256a
marcos <marc0sdurval20@gmail.com>
2022-02-25 09:32:44 -0300

    remove consoleLog

--- src/service/order.ts
+++ src/service/order.ts
@@ -2,7 +2,6 @@ import * as modelOrder from '../models/ordes';
 
 const createOrder = async (id:number, product:Array<number>) => {
   const [n, p] = await modelOrder.default(id, product);
-  console.log('N:', n, 'P:', p);
   return [n, p];
 };
 
