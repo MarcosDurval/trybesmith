bf37d4ba877c5a797e2e661d0decee627b393ade
marcos <marc0sdurval20@gmail.com>
2022-04-28 23:49:53 -0300

    refactor: renomeia arquivos

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -2,19 +2,12 @@ import { Router } from 'express';
 import 'express-async-errors';
 import * as controllerOrdes from '../controller/orders';
 import auth from '../controller/middleware/auth';
-import { IUser } from '../interface';
 
-// source: https://serveanswer.com/questions/how-do-i-add-custom-property-to-express-request-in-typescript
-declare module 'express-serve-static-core'{
-  interface Request {
-    user:IUser
-  }
-}
 const orders = Router();
 
 orders.use(auth);
 
-orders.post('/', controllerOrdes.default);
+orders.post('/', controllerOrdes.createOrder);
 
 orders.get('/', controllerOrdes.findAll);
 
