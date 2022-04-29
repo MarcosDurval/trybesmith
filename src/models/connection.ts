cfec5637b7aaaeab0b805cc389174a73fc810e9c
marcos <marc0sdurval20@gmail.com>
2022-04-29 00:08:41 -0300

    feat: conexão prisma

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,19 +1,12 @@
-import mysql2 from 'mysql2/promise';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD } = process.env;
-export const prisma = new PrismaClient({ datasources: {
+const prisma = new PrismaClient({ datasources: {
   db: {
-    url: `mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/Trybesmith?schema=public`,
+    url: process.env.DATABASE_URL,
   },
 } });
-const connection = mysql2.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-});
 
-export default connection;
+export default prisma;
