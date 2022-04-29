8dcb74c695ab242e4f4af9b347f91e8c2034afc0
marcos <marc0sdurval20@gmail.com>
2022-04-29 00:11:51 -0300

    refactor:  usa o this

--- src/error/customError.ts
+++ src/error/customError.ts
@@ -1,13 +1,11 @@
 import { ErrorType } from '../interface';
 
-// source: https://github.com/Microsoft/TypeScript-wiki/blob/main/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
 export default class CustomError extends Error {
   code:string;
 
   constructor({ code = 'Internal Server Error', message = 'inesperado' }:ErrorType) {
     super(message);
+    this.message = message;
     this.code = code;
-    // Set the prototype explicitly.
-    Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
