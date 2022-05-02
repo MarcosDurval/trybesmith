cc453616920e4ca90ab15feacaaed5d3c8617aa6
marcos <marc0sdurval20@gmail.com>
2022-05-01 22:34:03 -0300

    feat: adiciona busca por id em Products

--- src/models/product.ts
+++ src/models/product.ts
@@ -5,3 +5,5 @@ export const create = async (name:string, amount:string):Promise<IProducts> =>
   prisma.products.create({ data: { name, amount } });
 
 export const findProduct = async ():Promise<IProducts[]> => prisma.products.findMany();
+
+export const findId = async (id:IProducts['id']) => prisma.products.findFirst({ where: { id } });
--- src/service/products.ts
+++ src/service/products.ts
@@ -5,3 +5,7 @@ export const crate = async (name:string, amount:string):Promise<IProducts> =>
   modelProducts.create(name, amount);
 
 export const findAll = async ():Promise<IProducts[]> => modelProducts.findProduct();
+
+export const findId = async (id:IProducts['id'])
+:Promise<IProducts | null> => 
+  modelProducts.findId(id);
